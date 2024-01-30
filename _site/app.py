d93ff2e94d1a1a3dccb9df47933b43f26d1b04c4
Lin-cT <lindaytang8@gmail.com>
2024-01-30 07:59:16 +0000

    app.py

--- _site/app.py
+++ _site/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from user import user_api  # Importing the API blueprint from user.py
+from api.user import user_api  # Importing the API blueprint from user.py
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
@@ -8,10 +8,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Register the API blueprint
-app.register_blueprint(user_api)
+app.register_blueprint(user_api, url_prefix='/api/users')  # Registering the database blueprint with URL prefix
 
 if __name__ == "__main__":
-    from users import initUsers  # Assuming initUsers is defined in users.py
+    from model.users import initUsers  # Assuming initUsers is defined in model/users.py
     with app.app_context():
         db.create_all()
         initUsers()
